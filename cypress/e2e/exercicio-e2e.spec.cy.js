02a0a6559ac3e314d85af40cab36455090a34e04
Ryan Dulko <ryandulko458@gmail.com>
2024-06-05 18:23:28 -0300

    jekins

--- cypress/e2e/exercicio-e2e.spec.cy.js
+++ cypress/e2e/exercicio-e2e.spec.cy.js
@@ -12,7 +12,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
     beforeEach(() => {
         cy.visit('/')
-    });
+   });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
         cy.get('.icon-user-unfollow').click()
